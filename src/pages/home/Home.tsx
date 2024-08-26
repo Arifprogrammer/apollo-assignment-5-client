d7d70ae8b32855fee4c9edc0d4338b2588a523c6
Arifprogrammer <arifmgtcu@gmail.com>
2024-08-26 16:08:48 +0600

    implemented hero section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,16 @@
+import room from "../../assets/images/room-banner-edited.webp";
+
 const Home = () => {
-  return <main></main>;
+  return (
+    <main>
+      {/* //*Hero */}
+      <section className="pt-12 md:pt-24 bg-[#003049]">
+        <div>
+          <img src={room} alt="keyboard" className="size-full" />
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
