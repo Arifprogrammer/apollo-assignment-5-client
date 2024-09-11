ad388a16ddcf93883feb2edcc0e2a767d18a0267
Arifprogrammer <arifmgtcu@gmail.com>
2024-09-11 12:40:08 +0600

    changed baseapi

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -11,7 +11,7 @@ import { logout } from "../features/auth/authSlice";
 import Swal from "sweetalert2";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/v1",
+  baseUrl: "https://reserve-realm-server.vercel.app/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
